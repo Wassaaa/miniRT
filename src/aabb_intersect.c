d177cea9fe29e53b6c60ed0055d5719d54bbfcc2
aklein <aklein@student.hive.fi>
2024-07-24 17:48:05 +0300

    new aabb intersec function, all in 1 function and avoiding division etc

--- src/aabb_intersect.c
+++ src/aabb_intersect.c
@@ -1,41 +1,28 @@
 #include <miniRT.h>
 
-static double	calculate_t(double box_min, double box_max,
-						double ray_origin, double ray_inv_dir)
-{
-	double	t_min;
-	double	t_max;
-
-	t_min = (box_min - ray_origin) * ray_inv_dir;
-	t_max = (box_max - ray_origin) * ray_inv_dir;
-
-	return (fmax(fmin(t_min, t_max), 0.0));
-}
-
-static double	update_t(double t, double new_t)
-{
-	double	t_min;
-	double	t_max;
-
-	t_min = fmax(t, new_t);
-	t_max = new_t;
-
-	return (fmin(t_min, t_max));
-}
-
 bool	intersect_aabb(t_ray ray, t_aabb box, double max_t)
 {
-	double	t;
-	double	new_t;
+	double	t1;
+	double	t2;
+	double	t_min;
+	double	t_max;
 
-	t = calculate_t(box.min.x, box.max.x,
-				ray.origin.x, ray.inv_dir.x);
-	new_t = calculate_t(box.min.y, box.max.y,
-				ray.origin.y, ray.inv_dir.y);
-	t = update_t(t, new_t);
-	new_t = calculate_t(box.min.z, box.max.z,
-				ray.origin.z, ray.inv_dir.z);
-	t = update_t(t, new_t);
+	t1 = (box.min.x - ray.origin.x) * ray.inv_dir.x;
+	t2 = (box.max.x - ray.origin.x) * ray.inv_dir.x;
 
-	return (t < max_t);
+	t_min = fmin(t1, t2);
+	t_max = fmax(t1, t2);
+
+	t1 = (box.min.y - ray.origin.y) * ray.inv_dir.y;
+	t2 = (box.max.y - ray.origin.y) * ray.inv_dir.y;
+
+	t_min = fmax(t_min, fmin(t1, t2));
+	t_max = fmin(t_max, fmax(t1, t2));
+
+	t1 = (box.min.z - ray.origin.z) * ray.inv_dir.z;
+	t2 = (box.max.z - ray.origin.z) * ray.inv_dir.z;
+
+	t_min = fmax(t_min, fmin(t1, t2));
+	t_max = fmin(t_max, fmax(t1, t2));
+	return (t_max >= fmax(0.0, t_min) && t_min < max_t);
 }
